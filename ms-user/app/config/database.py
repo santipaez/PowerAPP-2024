2d67c70d7e14bce314fdc0cfdebba9ef9a53b360
santipaez <sr.paez@alumno.um.edu.ar>
2024-07-28 16:59:43 -0300

    Division de base de datos por cada microservicio

--- ms-user/app/config/database.py
+++ ms-user/app/config/database.py
@@ -2,13 +2,12 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-basedir = os.path.abspath(Path(__file__).parents[2])
+basedir = os.path.abspath(Path(__file__).parents[1])
 load_dotenv(os.path.join(basedir, '.env'))
 
-USER_DB=os.environ.get('USER_DB')
-PASS_DB=os.environ.get('PASS_DB')
-URL_DB=os.environ.get('URL_DB')
-NAME_DB=os.environ.get('NAME_DB')
-
+USER_DB=os.environ.get('DB_USER')
+PASS_DB=os.environ.get('DB_PASS')
+URL_DB=os.environ.get('DB_URL')
+NAME_DB=os.environ.get('DB_NAME')
 
 FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}:5432/{NAME_DB}'
